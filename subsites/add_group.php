8fa5a5804962ef29cc4b9ea31545e8592e7a20aa
woolenrainbow <janusadr@gmail.com>
2019-04-22 12:51:14 +0200

    Zmiana interakcji z bazą danych na prepared statements.

--- subsites/add_group.php
+++ subsites/add_group.php
@@ -8,7 +8,13 @@ $link = mysqli_connect("127.0.0.1", "root", "", "pz_projekt") or die(mysqli_conn
 mysqli_set_charset($link, "utf8");
 
 if (isset($_POST['nazwa_kola'])){
-    mysqli_query($link,"insert into groups (name, id_coordinator, description) values ('$_POST[nazwa_kola]', '$_POST[opiekun_kola]', '$_POST[opis_kola]')");
+    $_POST['nazwa_kola'] = mysqli_real_escape_string($link, $_POST['nazwa_kola']);
+    $_POST['opis_kola'] = mysqli_real_escape_string($link, $_POST['opis_kola']);
+    $_POST['opiekun_kola'] = mysqli_real_escape_string($link, $_POST['opiekun_kola']);
+    $stmt = mysqli_prepare($link, "insert into groups (name, id_coordinator, description) values ('$_POST[nazwa_kola]', '$_POST[opiekun_kola]', '$_POST[opis_kola]')");
+    mysqli_stmt_bind_param($stmt, "sis", $_POST['nazwa_kola'], $_POST['opis_kola'], $_POST['opiekun_kola']);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
 }
 header("location: ../websites/dodaj_kolo.php");
 ?>

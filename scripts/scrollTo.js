7e54c4a9b4f96cf18f3b6e62e5c90b58d4349cb1
Macherus <38405756+Macherus@users.noreply.github.com>
2019-05-25 00:03:25 +0200

    Update scrollTo.js
    Git nie zadziałał prawidłowo więc ponownie usuwam logi.

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -26,7 +26,6 @@
             sctrollTo(".dzialalnosc", -40);
         });
         $("#link3").click(function (e) {
-            console.log("dziala");
             sctrollTo(".group", -40);
         });
         $("#link4").click(function (e) {
